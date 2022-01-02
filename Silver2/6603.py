9ed5479dd9b12c1ec4b4c4edca744d8d0c8df0f1
woong99 <dndry1072@naver.com>
2022-01-02 14:16:00 +0900

    22.01.02 Baekjoon solution

--- Silver2/6603.py
+++ Silver2/6603.py
@@ -5,8 +5,31 @@
 정웅교
 문제: https://www.acmicpc.net/problem/6603
 """
+
+
+def sol(i):
+    if i == 6:
+        s = ' '.join(sorted(arr, key=int))
+        if s not in ans:
+            print(s)
+        ans.add(s)
+    else:
+        for j in range(n):
+            if not visited[j]:
+                arr[i] = ls[j]
+                visited[j] = True
+                sol(i + 1)
+                visited[j] = False
+
+
 while 1:
-    s = set(map(int, input().split()))
-    if len(s) == 1 and s[0] == 0:
+    ls = list(map(str, input().split()))
+    if len(ls) == 1 and ls[0] == '0':
         break
-    print(s)
+    n = int(ls[0])
+    del (ls[0])
+    visited = [False for _ in range(n)]
+    arr = [-1 for _ in range(6)]
+    ans = set()
+    sol(0)
+    print()
